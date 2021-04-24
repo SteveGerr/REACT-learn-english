642583d1a488bbc16d19f439baae214f6810ce12
SteveGerr <prikolsamara@mail.ru>
2021-04-24 22:27:34 +0400

    Add chatDialogItem

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,3 +1,4 @@
+import ChatDialogItem from '../ChatDialogItem/ChatDialogItem'
 import ChatList from '../ChatList/ChatList'
 import chatStyles from './Chat.module.scss'
 
@@ -13,6 +14,21 @@ const Chat = () => {
             {/* ChatDialogs */}
             <div className={chatStyles.chat__dialogs}>
                 <p className={chatStyles.chat__colHeading} >Dialogs</p>
+                <div className={chatStyles.dialogs_wrapper}>
+                    {/* LEft dialogs */}
+                    <div className={chatStyles.dialogs_left}>
+                        <ChatDialogItem />
+                    </div>
+                    <div className={chatStyles.dialogs_left}>
+                        <ChatDialogItem />
+                    </div>
+
+                    {/* Right dialogs */}
+                    <div className={chatStyles.dialogs_right}>
+                        <ChatDialogItem />
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
---
+++ src/components/ChatDialogItem/ChatDialogItem.jsx
@@ -0,0 +1,13 @@
+import cdiStyle from './ChatDialogItem.module.scss'    
+
+const ChatDialogItem = () => {
+
+    return (
+        <div className={cdiStyle.chat_dialog_item}>
+            <p>Hi, my name is Ivan</p>
+        </div>
+    )
+
+}
+
+export default ChatDialogItem
