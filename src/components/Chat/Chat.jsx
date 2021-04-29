63b8e81602fac8608b9aebe0bf4075ea3e22bf1a
SteveGerr <prikolsamara@mail.ru>
2021-04-29 22:17:38 +0400

    Routing

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,17 +1,12 @@
 import ChatDialogItem from '../ChatDialogItem/ChatDialogItem'
 import ChatInput from '../ChatInput/ChatInput'
-import ChatList from '../ChatList/ChatList'
 import chatStyles from './Chat.module.scss'
 
 const Chat = () => {
 
     return (
         <div className={chatStyles.chat}>
-            {/* ChatList */}
-            <div className={chatStyles.chat__list}>
-                <p className={chatStyles.chat__colHeading} >Choose the chat:</p>
-                <ChatList />
-            </div>
+
             {/* ChatDialogs */}
             <div className={chatStyles.chat__dialogs}>
                 <p className={chatStyles.chat__colHeading} >Dialogs</p>
--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,24 +1,36 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+import ChatList from '../ChatList/ChatList'
 import Chat from '../Chat/Chat'
 import WelcomeScreen from '../WelcomeScreen/WelcomeScreen'
 import msStyles from './MainScreen.module.scss'
 
 const MainScreen = () => {
-    let show = false
+    // let show = false
     return (
-        <div className={msStyles.MainScreen__wrapper}>
-            <header className={msStyles.MainScreen__header}>
-                <p className={msStyles.MainScreen__logo}>lengapp</p>
-            </header>
-            {/* Dinamic content */}
-            <section className={msStyles.MainScreen__content} >
-            {show ? <WelcomeScreen /> : null}
-            <Chat />
-            </section>
-            <footer className={msStyles.MainScreen__footer} >
-                <p>development by Ivan Shunyaev</p>
-            </footer>
+        <BrowserRouter>
+            <div className={msStyles.MainScreen__wrapper}>
+            
+                <header className={msStyles.MainScreen__header}>
+                    <p className={msStyles.MainScreen__logo}>lengapp</p>
+                </header>
+                {/* Dinamic content */}
+                <section className={msStyles.MainScreen__content} >
+                    <div className={msStyles.MainScreen__sidebar}>
+                        {/* <ChatList /> */}
+                        <ChatList />
+                    </div>
+                    <div className={msStyles.MainScreen__showContent}>
+                        <Route path="/" component={WelcomeScreen} />
+                        <Route path="/chats" component={Chat}/>
+                    </div>
+                    
+                </section>
+                <footer className={msStyles.MainScreen__footer} >
+                    <p>development by Ivan Shunyaev</p>
+                </footer>
 
-        </div>
+            </div>
+        </BrowserRouter>
     )
 }
 
